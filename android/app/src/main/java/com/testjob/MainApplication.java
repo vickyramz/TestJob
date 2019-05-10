d0a953abda4bbecc018f845448acc724dd240105
vicky ramz <fairvicks20@gmail.com>
2019-05-10 14:53:09 +0530

    Revert "My properties Map Configured"
    This reverts commit e7d8761012f93f7d0f4709292f8e8b9bd44b9540.

--- android/app/src/main/java/com/testjob/MainApplication.java
+++ android/app/src/main/java/com/testjob/MainApplication.java
@@ -3,7 +3,6 @@ package com.testjob;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.swmansion.reanimated.ReanimatedPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,7 +25,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new MapsPackage(),
             new ReanimatedPackage(),
             new RNGestureHandlerPackage()
       );
